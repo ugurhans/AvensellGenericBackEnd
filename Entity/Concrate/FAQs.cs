53487a3ac3fe3183fbed462a4583eeaab680c561
Enes Akgün <enes0303enes@gmail.com>
2023-12-25 18:15:19 +0300

    Faqs veri tabanı değişti

--- Entity/Concrate/FAQs.cs
+++ Entity/Concrate/FAQs.cs
@@ -6,10 +6,10 @@ namespace Entity.Concrate
     public class FAQs : IEntity
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Label { get; set; }
-        public string AddedDate { get; set; }
-        public string EditDate { get; set; }
+        public string? Desc { get; set; }
+        public string? Label { get; set; }
+        public DateTime? AddedDate { get; set; }
+        public DateTime? EditDate { get; set; }
     }
 }
 
