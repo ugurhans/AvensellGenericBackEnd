56e17c5e4957c12d292cf05b2225a05680509cdd
Enes Akgün <enes0303enes@gmail.com>
2023-12-26 09:43:20 +0300

    mail dosyasının canlıdaki yolu ve her kullanıcıya mail gitmesi için düzenleme

--- Business/Concrate/AuthManager.cs
+++ Business/Concrate/AuthManager.cs
@@ -113,7 +113,7 @@ namespace Business.Concrete
         {
             var code = GenerateRandomCode();
 
-            var result = await _mailService.SendLostEmailAsync(new Entity.Request.MailRequest() { Body = "Deneme", Subject = "Deneme", ToEmail = "ugurhanatilgan@gmail.com" });
+            var result = await _mailService.SendLostEmailAsync(new Entity.Request.MailRequest() { Body = "Deneme", Subject = "Deneme", ToEmail = userMail });
             if (result.Success)
             {
 
